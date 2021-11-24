3abbcfc575af2692f8fa4f8c26d6a9a06d272707
Mattouchou <90191409+Mattouchou@users.noreply.github.com>
2021-11-24 10:00:18 +0100

    chat

--- php/chat.php
+++ php/chat.php
@@ -1,39 +1,54 @@
 <?php
 include './gest_perso.php';
 ?>
-<fieldset>
 <legend>Parlez entre vous, les chiens galeux</legend>
 
 <form action="#" method="POST">
-
+<?php if(isset($errors)){
+            foreach ($errors as $message){
+                echo "<p class='errortest'> $message</p>";
+            }
+        }
+        ?>
 <div>
     <label for="message"> Entre ton message...</label>
-        <input type="textarea" name="message" id="message">
+        <textarea name="mess" id="mess"></textarea>
 <div>
     <button type="submit">Poster </button>
 </div>
     </div>
+
+
+</form>
+</fieldset>
+
 <?php
-if(!empty($POST)){
+if(!empty($_POST)){
     $errors = array();
 
   if(empty($_POST['mess'])){
       $errors['mess'] = "T'es tellement bourré que t'as rien écris ahaha !";
   }
-  $req = $pdo->prepare('INSERT INTO discut SET mess= ?, creadate = NOW()');
-  $req->execute([$_POST]['mess']);
-  $errors['mess'] = 'message enregistré';
+  $req = $pdo->prepare('INSERT INTO discut SET mess = ?, creadate = NOW()');
+  $req->execute([$_POST['mess']]);
+  $errors['mess'] = 'message non enregistré';
+}
+else{
+    $errors['formulaire'] = 'veuillez renseigner les champs ';
 }
 ?>
 
-</form>
-</fieldset>
 
-<!--<table>
+<table>
     <tr>
-        <th>Nom</th>
-        <br>
-        <th>Message</th>
+       
+        <?php
+
+        $req = $pdo->query('SELECT * FROM discut');
+        while($data = $req->fetch()){
+            echo "<tr><td>$username : </td><td>$data->mess</td><td>$data->creadate</td>";
+        }
+        ?>
 
     </tr>
 </table>
